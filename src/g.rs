2711901d261266e5a0a5915fba5743e3e6af0b61
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-24 17:42:32 +0100

    finish basic mysynth impl

--- src/g.rs
+++ src/g.rs
@@ -12,21 +12,34 @@ pub struct ComputeValue {
 pub struct Info {
     pub vals: Vec<Value>,
     pub min_size: usize,
+    pub ty: Ty,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Ty {
+    Int,
+    Bool,
 }
 
 impl Analysis<Term> for ComputeValue {
     type Data = Info;
 
     fn make(eg: &mut G, enode: &Term) -> Info {
-        let vals = eg.analysis.sigmas.iter().enumerate().map(|(i, sigma)|
+        let vals: Vec<_> = eg.analysis.sigmas.iter().enumerate().map(|(i, sigma)|
                     eval_step(enode, sigma, &|c| eg[c].data.vals[i].clone())
                 ).collect();
         let min_size = enode.children().iter().map(|c| eg[*c].data.min_size).sum::<usize>() + 1;
-        Info { vals, min_size }
+        let ty = vals.get(0).map(|x| match x {
+            Value::Int(_) => Ty::Int,
+            Value::Bool(_) => Ty::Bool,
+        }).unwrap_or(Ty::Int);
+        Info { vals, min_size, ty }
     }
 
     fn merge(&mut self, a: &mut Self::Data, b: Self::Data) -> DidMerge {
         assert!(&a.vals == &b.vals);
+        assert!(&a.ty == &b.ty);
+
         let a_size = a.min_size;
         let b_size = b.min_size;
         a.min_size = a_size.min(b_size);
--- src/mysynth.rs
+++ src/mysynth.rs
@@ -3,6 +3,7 @@ use crate::*;
 pub struct MySynth;
 
 pub type Score = usize;
+pub type PrioQueue = BinaryHeap<WithOrd<Id, Score>>;
 
 fn heuristic(info: &Info, sigmas: &[Sigma], problem: &impl Problem) -> Score {
     let counter = info.vals.iter().zip(sigmas.iter()).filter(|(val, sigma)|
@@ -21,22 +22,57 @@ impl Synth for MySynth {
             veccons: Default::default(),
         });
 
-        let mut queue: BinaryHeap<WithOrd<Id, Score>> = BinaryHeap::new();
+        // queue contains all the integer typed classes.
+        let mut queue: PrioQueue = BinaryHeap::new();
 
         for x in 0..problem.num_vars() {
-            let i = g.add(Term::Var(x));
-            let h = heuristic(&g[i].data, sigmas, problem);
-            queue.push(WithOrd(i, h));
+            add(Term::Var(x), &mut g, sigmas, problem, &mut queue);
         }
 
         loop {
             let WithOrd(i, _) = queue.pop().unwrap();
-            grow(&mut g, i);
+            grow(i, &mut g, sigmas, problem, &mut queue);
         }
     }
 }
 
-fn grow(g: &mut G, id: Id) {
-    // for each production rule add a term, where id is one child, and all the other children are variables.
-    todo!()
+fn grow(id: Id, g: &mut G, sigmas: &[Sigma], problem: &impl Problem, queue: &mut PrioQueue) {
+    if g[id].data.ty != Ty::Int { return; }
+
+    let vars: Vec<Id> = (0..problem.num_vars()).map(|x| g.add(Term::Var(x))).collect();
+    for &v in &vars {
+        add(Term::Add([id, v]), g, sigmas, problem, queue);
+        add(Term::Add([v, id]), g, sigmas, problem, queue);
+
+        add(Term::Sub([id, v]), g, sigmas, problem, queue);
+        add(Term::Sub([v, id]), g, sigmas, problem, queue);
+
+        add(Term::Mul([id, v]), g, sigmas, problem, queue);
+        add(Term::Mul([v, id]), g, sigmas, problem, queue);
+
+        add(Term::Div([id, v]), g, sigmas, problem, queue);
+        add(Term::Div([v, id]), g, sigmas, problem, queue);
+
+        for &v1 in &vars {
+            for &v2 in &vars {
+                let cond = g.add(Term::Lt([id, v]));
+                add(Term::Ite([cond, v1, v2]), g, sigmas, problem, queue);
+
+                let cond = g.add(Term::Lt([v, id]));
+                add(Term::Ite([cond, v1, v2]), g, sigmas, problem, queue);
+
+                let cond = g.add(Term::Lt([v, v1]));
+                add(Term::Ite([cond, id, v2]), g, sigmas, problem, queue);
+
+                let cond = g.add(Term::Lt([v, v1]));
+                add(Term::Ite([cond, v2, id]), g, sigmas, problem, queue);
+            }
+        }
+    }
+}
+
+fn add(term: Term, g: &mut G, sigmas: &[Sigma], problem: &impl Problem, queue: &mut PrioQueue) {
+    let i = g.add(term);
+    let h = heuristic(&g[i].data, sigmas, problem);
+    queue.push(WithOrd(i, h));
 }
