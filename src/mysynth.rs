09b7582d4f34e06402c0aa53173da903e97e498f
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-24 22:21:36 +0100

    bundle data into Ctxt object

--- src/mysynth.rs
+++ src/mysynth.rs
@@ -7,111 +7,120 @@ pub struct MySynth;
 type Score = usize;
 type PrioQueue = BinaryHeap<WithOrd<Id, Score>>;
 
-fn heuristic(info: &Info, sigmas: &[Sigma], problem: &impl Problem) -> Score {
-    let counter = satcount(info, sigmas, problem);
+struct Ctxt<'a, P: Problem> {
+    queue: PrioQueue,
+    sigmas: &'a [Sigma],
+    problem: &'a P,
+    g: G,
+}
 
-    const A: usize = 1;
-    const B: usize = 5;
+fn heuristic<'a, P: Problem>(info: &Info, ctxt: &Ctxt<'a, P>) -> Score {
+    let counter = satcount(info, ctxt);
 
     let mut a = 100000;
-    for _ in counter..sigmas.len() {
+    for _ in counter..ctxt.sigmas.len() {
         a /= 2;
     }
 
-    a / (info.min_size + B)
+    a / (info.min_size + 5)
 }
 
-fn satcount(info: &Info, sigmas: &[Sigma], problem: &impl Problem) -> usize {
+fn satcount<'a, P: Problem>(info: &Info, ctxt: &Ctxt<'a, P>) -> usize {
     let vals = info.vals.iter();
-    let sigmas = sigmas.iter();
-    let it = vals.zip(sigmas);
-    it.filter(|(val, sigma)| problem.sat(val, sigma))
+    let it = vals.zip(ctxt.sigmas);
+    it.filter(|(val, sigma)| ctxt.problem.sat(val, sigma))
       .count()
 }
 
 impl Synth for MySynth {
     fn synth(&mut self, problem: &impl Problem, sigmas: &[Sigma]) -> RecExpr<Term> {
-        let mut g = G::new(ComputeValue {
+        let g = G::new(ComputeValue {
             sigmas: sigmas.iter().cloned().collect(),
             veccons: Default::default(),
         });
-
-        // queue contains all the integer typed classes.
-        let mut queue: PrioQueue = BinaryHeap::new();
-
-        for x in 0..problem.num_vars() {
-            add(Term::Var(Var(x)), &mut g, sigmas, problem, &mut queue);
-        }
-
-        loop {
-            let WithOrd(i, score) = queue.pop().unwrap();
-            let cnt = satcount(&g[i].data, sigmas, problem);
-            let b = cnt == sigmas.len();
-            if b || DEBUG {
-                let e = Extractor::new(&g, AstSize);
-                let (ast_size, t) = e.find_best(i);
-                if DEBUG {
-                    let sigs = sigmas.len();
-                    let ast2 = g[i].data.min_size;
-                    println!("[{sigs}] sc={score} sat={cnt} ast1={ast_size} ast2={ast2} {t}");
-                }
-                if b {
-                    return t;
-                }
-            }
-            grow(i, &mut g, sigmas, problem, &mut queue);
-        }
+        let mut ctxt = Ctxt {
+            problem,
+            sigmas,
+            g,
+            queue: Default::default(),
+        };
+        run(&mut ctxt)
     }
 }
 
-fn grow(id: Id, g: &mut G, sigmas: &[Sigma], problem: &impl Problem, queue: &mut PrioQueue) {
-    assert!(g[id].data.ty == Ty::Int);
+fn run<'a, P: Problem>(ctxt: &mut Ctxt<'a, P>) -> RecExpr<Term> {
+    for x in 0..ctxt.problem.num_vars() {
+        add(Term::Var(Var(x)), ctxt);
+    }
+
+    loop {
+        let WithOrd(i, score) = ctxt.queue.pop().unwrap();
+        let cnt = satcount(&ctxt.g[i].data, ctxt);
+        let b = cnt == ctxt.sigmas.len();
+        if b || DEBUG {
+            let e = Extractor::new(&ctxt.g, AstSize);
+            let (ast_size, t) = e.find_best(i);
+            if DEBUG {
+                let sigs = ctxt.sigmas.len();
+                let ast2 = ctxt.g[i].data.min_size;
+                println!("[{sigs}] sc={score} sat={cnt} ast1={ast_size} ast2={ast2} {t}");
+            }
+            if b {
+                return t;
+            }
+        }
+        grow(i, ctxt);
+    }
+}
+
+fn grow<'a, P: Problem>(id: Id, ctxt: &mut Ctxt<'a, P>) {
+    assert!(ctxt.g[id].data.ty == Ty::Int);
 
     // Why does this happen?
-    if g[id].data.already_grown { return; }
+    if ctxt.g[id].data.already_grown { return; }
 
-    let vars: Vec<Id> = (0..problem.num_vars()).map(|x| g.add(Term::Var(Var(x)))).collect();
+    let vars: Vec<Id> = (0..ctxt.problem.num_vars()).map(|x| ctxt.g.add(Term::Var(Var(x)))).collect();
     for &v in &vars {
-        add(Term::Add([id, v]), g, sigmas, problem, queue);
-        add(Term::Add([v, id]), g, sigmas, problem, queue);
+        add(Term::Add([id, v]), ctxt);
+        add(Term::Add([v, id]), ctxt);
 
-        add(Term::Sub([id, v]), g, sigmas, problem, queue);
-        add(Term::Sub([v, id]), g, sigmas, problem, queue);
+        add(Term::Sub([id, v]), ctxt);
+        add(Term::Sub([v, id]), ctxt);
 
-        add(Term::Mul([id, v]), g, sigmas, problem, queue);
-        add(Term::Mul([v, id]), g, sigmas, problem, queue);
+        add(Term::Mul([id, v]), ctxt);
+        add(Term::Mul([v, id]), ctxt);
 
-        add(Term::Div([id, v]), g, sigmas, problem, queue);
-        add(Term::Div([v, id]), g, sigmas, problem, queue);
+        add(Term::Div([id, v]), ctxt);
+        add(Term::Div([v, id]), ctxt);
 
         for &v1 in &vars {
             for &v2 in &vars {
-                let cond = g.add(Term::Lt([id, v]));
-                add(Term::Ite([cond, v1, v2]), g, sigmas, problem, queue);
+                let cond = ctxt.g.add(Term::Lt([id, v]));
+                add(Term::Ite([cond, v1, v2]), ctxt);
 
-                let cond = g.add(Term::Lt([v, id]));
-                add(Term::Ite([cond, v1, v2]), g, sigmas, problem, queue);
+                let cond = ctxt.g.add(Term::Lt([v, id]));
+                add(Term::Ite([cond, v1, v2]), ctxt);
 
-                let cond = g.add(Term::Lt([v, v1]));
-                add(Term::Ite([cond, id, v2]), g, sigmas, problem, queue);
+                let cond = ctxt.g.add(Term::Lt([v, v1]));
+                add(Term::Ite([cond, id, v2]), ctxt);
 
-                let cond = g.add(Term::Lt([v, v1]));
-                add(Term::Ite([cond, v2, id]), g, sigmas, problem, queue);
+                let cond = ctxt.g.add(Term::Lt([v, v1]));
+                add(Term::Ite([cond, v2, id]), ctxt);
             }
         }
     }
 
-    let mut info = g[id].data.clone();
+    let mut info = ctxt.g[id].data.clone();
     info.already_grown = true;
-    g.set_analysis_data(id, info);
+    ctxt.g.set_analysis_data(id, info);
 }
 
-fn add(term: Term, g: &mut G, sigmas: &[Sigma], problem: &impl Problem, queue: &mut PrioQueue) {
-    let i = g.add(term);
-    let info = &g[i].data;
+fn add<'a, P: Problem>(term: Term, ctxt: &mut Ctxt<'a, P>) {
+    let i = ctxt.g.add(term);
+    let info = &ctxt.g[i].data;
 
     if info.ty == Ty::Int && !info.already_grown {
-        let h = heuristic(&g[i].data, sigmas, problem);
-        queue.push(WithOrd(i, h));
+        let h = heuristic(&ctxt.g[i].data, ctxt);
+        ctxt.queue.push(WithOrd(i, h));
     }
 }
--- src/with_ord.rs
+++ src/with_ord.rs
@@ -1,10 +1,8 @@
-use crate::*;
-
 use std::cmp::Ordering;
 
 // Takes the `Ord` from U, but reverses it.
 #[derive(PartialEq, Eq, Debug)]
-pub(crate) struct WithOrd<T: Eq, U: Ord>(pub T, pub U);
+pub struct WithOrd<T: Eq, U: Ord>(pub T, pub U);
 
 impl<T: Eq, U: Ord> PartialOrd for WithOrd<T, U> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
