7118e903f74b299bb433006151b989e998872f5f
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-29 14:27:28 +0100

    add max4, and setup flamegraph stuff

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,6 @@ pub type Set<V> = std::collections::HashSet<V>;
 pub use std::collections::BinaryHeap;
 
 fn main() {
-    let (problem, oracle) = sygus_problem("qm_neg_eq_2.sl");
+    let (problem, oracle) = sygus_problem("max4.sl");
     println!("Answer: {:?}", cegis(problem, MySynth, oracle));
 }
