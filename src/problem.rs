43fb59ca9c54ff584c79d0e35b0c53ba25849677
memoryleak47 <memoryleak47@protonmail.ch>
2025-07-31 18:42:34 +0100

    ...

--- src/problem.rs
+++ src/problem.rs
@@ -106,6 +106,9 @@ fn sygus_expr_to_term_impl(e: Expr, vars: &IndexMap<String, Ty>, progname: &str,
 pub fn mk_sygus_problem(f: &str) -> Problem {
     let s = std::fs::read_to_string(f).unwrap();
 
+    dbg!(parse_synth(&s));
+    std::process::exit(0);
+
     let (parsed, _) = parse_sygus(&s).unwrap();
 
     build_sygus(parsed)
