24c0ac900099fd2cc2079b2939da6b2bf5269101
memoryleak47 <memoryleak47@protonmail.ch>
2025-08-01 09:10:04 +0100

    ...

--- runall.sh
+++ runall.sh
@@ -3,12 +3,14 @@
 cargo b --release
 
 function run() {
+    t=$(mktemp -d)
+    cp target/release/liasynth $t
     for f in $(find "examples/LIA" -type f)
     do
         echo
         echo ==========
         echo "$f:"
-        timeout 10s target/release/liasynth "$f"
+        timeout 10s $t/liasynth "$f"
     done
 
     success=$(cat bench.txt | grep Answer | wc -l)
