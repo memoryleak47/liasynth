f859562fe036c96a2ac8bde6c5bc79207458dd2f
guy <guy.h.frankel@gmail.com>
2025-09-30 17:05:12 +0100

    testing on LIA with whole (cleaned) grammars

--- genresults.py
+++ genresults.py
@@ -48,10 +48,10 @@ def run_lia_cvc5(benchmark, results, timeout=10):
     clen_path = f"examples/cleaned-LIA/{benchmark}"
 
     cmds = {
-        "liasynth":       f'python python_frontend.py {lia_path} && RUSTFLAGS="-Awarnings" cargo run -j4 --release -- {lia_path}',
-        "cvc5":           f'cvc5 --lang=sygus2 {lia_path}',
-        # "cvc5_clean":     f'cvc5 --lang=sygus2 {clen_path}',
-        # "liasynth_clean": f'python python_frontend.py {clen_path} && RUSTFLAGS="-Awarnings" cargo run -j4 --release -- {clen_path}',
+        # "liasynth":       f'python python_frontend.py {lia_path} && RUSTFLAGS="-Awarnings" cargo run -j4 --release -- {lia_path}',
+        # "cvc5":           f'cvc5 --lang=sygus2 {lia_path}',
+        "liasynth_clean": f'python python_frontend.py {clen_path} && RUSTFLAGS="-Awarnings" cargo run -j4 --release -- {clen_path}',
+        "cvc5_clean":     f'cvc5 --lang=sygus2 {clen_path}',
     }
 
     with ThreadPoolExecutor(max_workers=2) as ex:
@@ -81,25 +81,27 @@ def run_lia_cvc5(benchmark, results, timeout=10):
 
             if 'infeasible' in out:
                 results[name]['status'].append('infeasible')
-            else::
+            else:
                 results[name]['status'].append(out)
             results[name]['solution'].append('-')
 
 def run_all(folder, *, timeout):
     results = defaultdict(lambda: defaultdict(list))
-    for fname in os.listdir(folder):
+    l = len(os.listdir(folder)
+    for i, fname in enumerate(os.listdir(folder), start=1):
         run_lia_cvc5(fname, results, timeout)
         print(
+            f"[{i}/{l}]",
             fname,
-            f"\n\tcvc5: {results['cvc5']['status'][-1]}",
-            # f"\n\tcvc5_clean: {results['cvc5_clean']['status'][-1]}",
-            f"\n\tlia: {results['liasynth']['status'][-1]}",
-            # f"\n\tlia_clean: {results['liasynth_clean']['status'][-1]}",
+            # f"\n\tcvc5: {results['cvc5']['status'][-1]}",
+            f"\n\tcvc5_clean: {results['cvc5_clean']['status'][-1]}",
+            # f"\n\tlia: {results['liasynth']['status'][-1]}",
+            f"\n\tlia_clean: {results['liasynth_clean']['status'][-1]}",
         )
 
-    pd.DataFrame.from_dict(results['cvc5']).to_csv('cvc5_LIA_res.csv', index=False)
-    # pd.DataFrame.from_dict(results['cvc5_clean']).to_csv('cvc5_cLIA_res.csv', index=False)
-    pd.DataFrame.from_dict(results['liasynth']).to_csv('lia_LIA_res.csv', index=False)
-    # pd.DataFrame.from_dict(results['liasynth_clean']).to_csv('lia_cLIA_res.csv', index=False)
+    # pd.DataFrame.from_dict(results['cvc5']).to_csv('cvc5_LIA_res.csv', index=False)
+    pd.DataFrame.from_dict(results['cvc5_clean']).to_csv('results/cvc5_cLIA_res.csv', index=False)
+    # pd.DataFrame.from_dict(results['liasynth']).to_csv('lia_LIA_res.csv', index=False)
+    pd.DataFrame.from_dict(results['liasynth_clean']).to_csv('results/lia_cLIA_res.csv', index=False)
 
 run_all('examples/LIA', timeout=10)
