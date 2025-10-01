81529ebbd0c12e100e6cde17a0c8052b2267f344
memoryleak47 <memoryleak47@protonmail.ch>
2025-10-01 16:17:31 +0200

    update flame.sh to use python_frontend.py

--- flame.sh
+++ flame.sh
@@ -1,1 +1,3 @@
-RUSTFLAGS="-C force-frame-pointers=yes" cargo flamegraph
+f=examples/LIA/array_search_10.sl
+python3 python_frontend.py "$f"
+RUSTFLAGS="-C force-frame-pointers=yes" cargo flamegraph -- "$f"
