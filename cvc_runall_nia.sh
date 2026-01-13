bc4dcd018c8b73d6504f1a269dc5f8add8aebc6e
guy <guy.h.frankel@gmail.com>
2026-01-13 20:28:42 +0000

    cvc_rf_runall

---
+++ cvc_runall_nia.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0  <directory> <outfile>"
+    exit 1
+fi
+
+indir="$1"
+outfile="$2"
+
+all=$(find "$indir" -type f -name "*.sl" | wc -l)
+function run() {
+    i=0
+    t=$(mktemp -d)
+    for f in $(find "$indir" -type f -name "*.sl" | sort); do
+        echo
+        echo ==========
+        echo "[$i/$all] $f:"
+        timeout 10s cvc5 "$f" --force-logic=NIA
+        i=$(($i+1))
+    done
+
+    success=$(cat $outfile | grep define-fun | wc -l)
+
+    echo
+    echo "Completed $success/$all"
+}
+
+run 2>&1 | tee $outfile
